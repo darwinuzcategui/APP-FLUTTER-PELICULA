b27e987b5ed1cddc7a88cd52e495c8c77eb5d1f4
Darwin Uzcategui <darwin.uzcategui1973@gmail.com>
2023-02-01 11:16:02 -0400

    datos

--- android/app/src/main/kotlin/com/darwinuzcategui/peliculas/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.darwinuzcategui.peliculas
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/darwinuzcategui/productosdx3d/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.darwinuzcategui.productosdx3d
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
